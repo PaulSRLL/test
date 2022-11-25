405ea7bb5b3c2c127fbd15fbd87c998a69007e78
PaulSRLL <118457384+PaulSRLL@users.noreply.github.com>
2022-11-25 09:57:22 +0000

    Add files via upload

---
+++ qgis2web_2022_11_25-09_55_31_682592/data/SiteLocation_2.js
@@ -0,0 +1,1 @@
+var json_SiteLocation_2 = {"type":"FeatureCollection","name":"SiteLocation_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[-1.638682920227526,52.779511851172167]}}]}
